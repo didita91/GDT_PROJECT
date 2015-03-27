b6991e910a75cfec001822ef7072c912b1cb6347
Teresita <correo@correo.com>
2015-03-27 01:31:12 -0400

    Configuracion de la base.

--- gdt_project/settings.py
+++ gdt_project/settings.py
@@ -1,4 +1,4 @@
-# Django settings for gdp project.
+# Django settings for GDT project.
 # -*- coding: iso-8859-15 -*-
 import os.path
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -10,13 +10,6 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-
-#DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-#DATABASE_NAME = 'gdtdb'             # Or path to database file if using sqlite3.
-#DATABASE_USER = 'postgres'             # Not used with sqlite3.
-#DATABASE_PASSWORD = '12345'         # Not used with sqlite3.
-#DATABASE_HOST = 'localhost'             # Set to empty string for localhost. Not used with sqlite3.
-#DATABASE_PORT = '5432'             # Set to empty string for default. Not used with sqlite3.
 
 DATABASES = {
     'default': {
