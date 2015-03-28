d8412f378a26be2e90ef140203114ae6867156cb
didita91 <didif.91@gmail.com>
2015-03-27 21:50:10 -0400

    eliminacion de carpeta

--- app/helper.py
+++ app/helper.py
@@ -28,15 +28,7 @@ def obtener_relaciones_der(itm, lista_existentes):
                 ret.extend(aux)
     return ret
 
-def get_permisos_proyecto(user, proyecto):
-    roles = UsuarioRolProyecto.objects.filter(usuario = user, proyecto = proyecto).only('rol')
-    permisos_obj = []
-   # for i in roles:
-    #    permisos_obj.extend(i.rol.permisos.filter(rolpermiso__fase = proyecto.fase))
-    permisos = []
-    for i in permisos_obj:
-        permisos.append(i.nombre)
-    return permisos
+
 
 	
 
