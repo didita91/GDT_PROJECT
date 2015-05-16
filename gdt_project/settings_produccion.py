e9c9d9af1e097597f8195b13cfdc08632aee7814
didita91 <didif.91@gmail.com>
2015-05-15 23:22:34 -0400

    settings

--- gdt_project/settings_produccion.py
+++ gdt_project/settings_produccion.py
@@ -46,7 +46,7 @@ USE_I18N = True
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
 #MEDIA_ROOT = RUTA_PROYECTO.child('site_media'),
-MEDIA_ROOT = RUTA_PROYECTO
+MEDIA_ROOT = "/var/www/gdt_project/documentos"
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
